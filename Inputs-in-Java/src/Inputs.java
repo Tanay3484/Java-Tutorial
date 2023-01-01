82a9b9c18200d07e5055607d1034957c647b2de1
tanay3484 <tanay8705@gmail.com>
2023-01-01 15:41:39 +0530

    I/O in Java

---
+++ Inputs-in-Java/src/Inputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your age : ");
+        int age = input.nextInt();
+        System.out.println("You have entered your age to be : "+age);
+    }
+}
