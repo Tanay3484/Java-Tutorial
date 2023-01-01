71a2fe24ebc9a93ee5438bf0e007211565e30c46
tanay3484 <tanay8705@gmail.com>
2023-01-01 18:49:56 +0530

    Datatypes in Java

---
+++ Datatypes/src/Primitives.java
@@ -0,0 +1,27 @@
+//package Datatypes.src;
+
+public class Primitives {
+    public static void main(String[] args) {
+        
+        //integer data type
+        int rollno = 64;
+
+        //character data type
+        char letter = 'r';
+
+        //float data type
+        float marks = 98.67f;
+        
+        //double data type
+        double largeDecimalNumber = 234723648723.23423874923;
+
+        //long data type
+        long largeIntegerNumber = 779473210342342312l;
+
+        //boolean data type
+        boolean check = true; //or false
+
+        //String Data type
+        String name = "John";
+    }
+}
